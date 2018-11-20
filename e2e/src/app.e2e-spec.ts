ac558396396a541b815de5ea71e3920729a8139f
Krzysztof Kuriata <krzysztof@kuriata.me>
2018-11-20 01:20:11 +0100

    Tests fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to poll-wizard!');
+    expect(page.getTitleText()).toEqual('Welcome to POLL-WIZARD!');
   });
 });
